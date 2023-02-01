7ade86cf09901ec0c0f764ba7ebfcc18ab8f53b8
Hiroshi Igarashi <hirotech.net@gmail.com>
2023-02-01 19:02:23 +0900

    viteをインストールしてhelloworldでお試し

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 import './bootstrap';
+
+const sayHello = () => {
+  console.log("Hello");
+};
+
+document.getElementById('app').innerHTML = '<span style="background-color:red;">Hello World</span>';
+sayHello();
+
